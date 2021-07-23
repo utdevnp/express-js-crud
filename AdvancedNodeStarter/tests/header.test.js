c79b5f485259a7d96631faafb0eb0c367214c008
Utshab Luitel <utdevnp@gmail.com>
2021-07-23 11:08:57 +0545

    add beforeEach for negivate in project first of the test

--- AdvancedNodeStarter/tests/header.test.js
+++ AdvancedNodeStarter/tests/header.test.js
@@ -1,22 +1,22 @@
 const puppeteer = require("puppeteer");
-
-
-test("Add two numbers ", ()=>{
-    const sum = 2+3;
-    expect(sum).toEqual(5);
-});
-
-// lunch browser
-test("We can lunch browser", async ()=>{
+let browser,page;
+beforeEach(async ()=>{
     // create a browser using 'puppeteer'
-    const browser = await puppeteer.launch({
+    browser = await puppeteer.launch({
         headless:false
     });
 
     // create a page like a new tab
-    const page = await browser.newPage();
-
+    page = await browser.newPage();
+   
     // open our project 
     await page.goto("localhost:3000");
+})
 
+// lunch browser
+test("We can lunch browser", async ()=>{
+    // find logo text in the site 
+    const text = await page.$eval("a.brand-logo", el=>el.innerHTML);
+    // expect text is equal to Blogster
+    expect(text).toEqual("Blogster");
 });
