1975ed13359b496fe44dc1e0492ddd2f7727cd6e
Utshab Luitel <utdevnp@gmail.com>
2021-07-23 12:36:02 +0545

    click login start flow with page.click

--- AdvancedNodeStarter/tests/header.test.js
+++ AdvancedNodeStarter/tests/header.test.js
@@ -1,6 +1,11 @@
 const puppeteer = require("puppeteer");
 let browser,page;
 beforeEach(async ()=>{
+    // if we get given message on test 
+    // Timeout - Async callback was not invoked within the 5000ms timeout specified by jest.setTimeout.
+    // please set jest timeout 
+    jest.setTimeout(10000);
+
     // create a browser using 'puppeteer'
     browser = await puppeteer.launch({
         headless:false
@@ -13,10 +18,26 @@ beforeEach(async ()=>{
     await page.goto("localhost:3000");
 })
 
-// lunch browser
-test("We can lunch browser", async ()=>{
-    // find logo text in the site 
+
+
+afterEach( async ()=>{
+    await browser.close();
+    jest.clearAllTimers();
+})
+
+// check heder test is correct or not 
+test("Header has correct text",async ()=>{
     const text = await page.$eval("a.brand-logo", el=>el.innerHTML);
-    // expect text is equal to Blogster
     expect(text).toEqual("Blogster");
+})
+
+
+test("click login start oauth flow", async ()=>{
+    // find aotuh link the site 
+    await page.click('.right a');
+
+    // expect url is to be match
+    const url = await page.url()
+    console.log(url);
 });
+
