3647f469b403a5feb15914701b834d0f73044586
Utshab Luitel <utdevnp@gmail.com>
2021-07-28 16:50:34 +0545

    array object adding and removing in the collection

--- course-project/other/arrayofDocument.js
+++ course-project/other/arrayofDocument.js
@@ -10,7 +10,7 @@ const authorSchema = new db.Schema({
 
 const courseSchema = new db.Schema({
     name: String,
-    author : authorSchema, // db.Schema.Types.ObjectId,
+    authors : [authorSchema], // db.Schema.Types.ObjectId,
     price: Number,
     tags: Array,
     isPublish: Boolean
@@ -24,7 +24,7 @@ async function createCourse(name,author){
 
     let courseAdd = new Course({
         name: name,
-        author: author,
+        authors: author,
         proce: "10",
         isPublish:true,
         tags: ['js','es']
@@ -67,13 +67,42 @@ async function updateAuthorByUsingSet(courseId){
     console.log("updated...");
 }
 
+
+async function addAuthors(courseId, author){
+    const course = await Course.findById(courseId);
+    course.authors.push(author);
+    course.save();
+
+    console.log("updated...");
+}
+
+async function removeAuthor(courseId,authorId){
+    const course = await Course.findById(courseId);
+    const author = course.authors.id(authorId);
+    author.remove();
+    course.save();
+
+    console.log("... updated");
+}
+
 //createCourse("this is test course","610112a8f1327d0fb865797f")
 
 // next method create with author schema
-//createCourse("this is test course",new Author({name:"bastolag"}))
+// createCourse("this is test course",[
+//     new Author({name:"bastolag"}),
+//     new Author({name:"rabing"}),
+//     new Author({name:"nepalg"})
+// ])
+
+
 
 //getCourse("6101247ce21f0d42f44e38b5");
 
 //updateAuthor("6101247ce21f0d42f44e38b5");
 
-updateAuthorByUsingSet('6101247ce21f0d42f44e38b5');
+//updateAuthorByUsingSet('6101247ce21f0d42f44e38b5');
+
+//addAuthors("610137a02da7f83214d395a8", new Author({name:"balkon"}) )
+
+
+removeAuthor("610137a02da7f83214d395a8","610137a02da7f83214d395a7");
